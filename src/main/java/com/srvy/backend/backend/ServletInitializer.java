a834bc12b6bc44925ab43a28d22df8e178292317
= <=>
2018-01-21 09:29:13 +0530

    Boilerplate for code starting appengine on spring boot project

---
+++ src/main/java/com/srvy/backend/backend/ServletInitializer.java
@@ -0,0 +1,11 @@
+package com.srvy.backend.backend;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		  return application.sources(BackendApplication.class);
+	  }
+}
